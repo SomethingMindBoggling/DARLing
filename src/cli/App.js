a029167da56bd5ba91e2021132e0bdeeaef3f11b
Jacob Windsor <jacob.windsor@ymail.com>
2017-12-14 22:26:27 +0000

    Add toolbar

--- src/cli/App.js
+++ src/cli/App.js
@@ -1,1 +1,24 @@
-export default () => <div>Hello</div>
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
+
+export default class extends Component {
+  render() {
+    return (
+      <MuiThemeProvider>
+        <Toolbar>
+          <ToolbarGroup>
+            <ToolbarTitle text="DARLing"/>
+            <FlatButton label="Datasets" />
+          </ToolbarGroup>
+          <ToolbarGroup>
+            <ToolbarSeparator/>
+            <RaisedButton label="Rank a dataset" primary={true} />
+          </ToolbarGroup>
+        </Toolbar>
+      </MuiThemeProvider>
+    );
+  }
+}
