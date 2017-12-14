8c49ea89971d48afe2ddd2d8c1f85dd34198945f
Jacob Windsor <me@jcbwndsr.com>
2017-12-14 22:36:29 +0000

    Add routing

--- src/cli/App.js
+++ src/cli/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import RaisedButton from 'material-ui/RaisedButton';
 import FlatButton from 'material-ui/FlatButton';
+import Datasets from './scenes/datasets';
 
 export default class extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <Toolbar>
-          <ToolbarGroup>
-            <ToolbarTitle text="DARLing"/>
-            <FlatButton label="Datasets" />
-          </ToolbarGroup>
-          <ToolbarGroup>
-            <ToolbarSeparator/>
-            <RaisedButton label="Rank a dataset" primary={true} />
-          </ToolbarGroup>
-        </Toolbar>
-      </MuiThemeProvider>
+      <Router>
+        <MuiThemeProvider>
+          <Toolbar>
+            <ToolbarGroup>
+              <ToolbarTitle text="DARLing"/>
+              <Link to="/">
+                <FlatButton label="Datasets" />
+              </Link>
+            </ToolbarGroup>
+            <ToolbarGroup>
+              <ToolbarSeparator/>
+              <RaisedButton label="Rank a dataset" primary={true} />
+            </ToolbarGroup>
+          </Toolbar>
+
+          <Route exact path="/" component={Datasets} />
+        </MuiThemeProvider>
+      </Router>
     );
   }
 }
